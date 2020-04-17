224ea0b410054e0728642515727f06627e138042
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-17 12:51:17 -0700

    test pass for calculator object

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,10 +1,10 @@
 import { Calculator } from './../src/backend-calc.js'
 
 
-describe ("g-calculator", () => {
+describe ("Calculator", () => {
 
-test("it should create a new g-calculator object" , () => {
-  let calculator = new g-calculator;
+test("it should create a new Calculator object" , () => {
+  let calculator = new Calculator;
   expect(calculator).toEqual({})
 })
 })
