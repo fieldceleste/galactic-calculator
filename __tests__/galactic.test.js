701c69c9f852e01411fb36f6ca69d211baeb07ba
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-17 12:43:53 -0700

    fail test - g is not defined

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,3 +1,10 @@
-import { Calculator, User } from './../src/backend-calc.js'
-import { Person } from './../src/backend-calc.js'
+import { Calculator } from './../src/backend-calc.js'
 
+
+describe ("g-calculator", () => {
+
+test("it should create a new g-calculator object" , () => {
+  let calculator = new g-calculator;
+  expect(calculator).toEqual({})
+})
+})
