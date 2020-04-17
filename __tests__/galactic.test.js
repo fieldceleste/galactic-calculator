de69074e8739c4651d0c249c36777a2732cb72d3
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-04-17 16:18:03 -0700

    test pass for mercury age

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -8,20 +8,21 @@ describe ("User", () => {
 
   beforeEach(() => {
     ageInput = 30;
-    user= new User(ageInput);
+    user = new User(ageInput);
 
   });
-test("it should create a object called user", () => {
-  let user = new User;
-  expect(user).toEqual({})
-});
+// test("it should create a object called user", () => {
+//   let user = new User;
+//   expect(user).toEqual({})
+// });
 test ("it should use the object user and calculate earth age", () => {
+  expect(new User()).toBeInstanceOf(User);
   expect(user.earthA).toEqual(30);
 });
 test ("it should show users inputted age in Mercury years" , () => {
-  expect(user.mercuryA).toEqual(48.4);
+  expect(user.mercuryA).toEqual(125);
 
-})
+  });
 });
 
 
